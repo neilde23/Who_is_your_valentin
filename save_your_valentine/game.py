669ac952dbe7f534b0723166d6ad96a76d42f1dc
neilde23 <neilah.abdou-imorou@epitech.eu>
2023-02-12 23:40:34 +0100

    update game and add some questions

--- save_your_valentine/game.py
+++ save_your_valentine/game.py
@@ -39,37 +39,59 @@ background = pygame.transform.scale(background, window_size)
 # Display background
 window.blit(background, (0, 0))
 
-# Display question and answers
-question = "Quel est le nom du super-héros qui a les ailes d'un cygne et la force d'un lion ?"
-draw_text(question, width //2, height //2 - 100)
+# Quiz questions and answers
+quiz = [
+    {
+        "question": "Quel est le nom du super-héros qui a les ailes d'un cygne et la force d'un lion ?",
+        "answers": ["Wonder Woman", "Superman", "Cyclone", "Hawkwoman"],
+        "correct": "Hawkwoman"
+    },
+    {
+        "question": "Quel est le nom du super-héros qui peut voler, a une vitesse surhumaine et une force surhumaine ?",
+        "answers": ["Batman", "Superman", "Wonder Woman", "Cyclone"],
+        "correct": "Superman"
+    }
+]
 
-answers = ["Wonder Woman", "Superman", "Cyclone", "Hawkwoman"]
-answer_rects = []
-x = 85
-for i, answer in enumerate(answers):
-    x += 250
-    y = height // 2
-    if x > width - 250:
-        x = 600
-        y = height // 2 + 150
-    answer_rect = pygame.draw.rect(window, (255, 215, 0), (x-100, y-25, 200, 50), 2, border_radius=10)
-    draw_text(answer, x, y)
-    answer_rects.append(answer_rect)
+# Keep track of the number of correct answers
+correct_answers = 0
 
-pygame.display.update()
+# Loop through the quiz questions
+for i, question_dict in enumerate(quiz):
+    # Clear the screen
+    window.fill((255, 255, 255))
+    
+    # Draw the question
+    question = question_dict["question"]
+    draw_text(question, width //2, height //2 - 100)
+    
+    # Draw the answers
+    answers = question_dict["answers"]
+    answer_rects = []
+    x = 85
+    for i, answer in enumerate(answers):
+        x += 250
+        y = height // 2
+        if x > width - 250:
+            x = 600
+            y = height // 2 + 150
+        answer_rect = pygame.draw.rect(window, (255, 215, 0), (x-100, y-25, 200, 50), 2, border_radius=10)
+        draw_text(answer, x, y)
+        answer_rects.append(answer_rect)
 
-# Wait for user to click on an answer
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            for i, answer_rect in enumerate(answer_rects):
-                if answer_rect.collidepoint(mouse_x, mouse_y):
-                    print("Vous avez sélectionné la réponse", answers[i])
-
-
-# Start the quiz
-start_quiz()
+    pygame.display.update()
+    
+    # Wait for user to click on an answer
+    user_answer = None
+    while not user_answer:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for k, answer_rect in enumerate(answer_rects):
+                    if answer_rect.collidepoint(mouse_x, mouse_y):
+                        print("Vous avez sélectionné la réponse", answers[k])
+                        break
+                break
